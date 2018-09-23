e9c040441f7ed81cc1c86444bf872e466bc4810a
Ivan <ivanmikhailenka1@gmail.com>
2018-09-23 18:16:17 +0300

    implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+class BracketPair {
+
+    constructor(startBracket, endBracket) {
+        this.startBracket = startBracket;
+        this.endBracket = endBracket;
+    }
+
 }
+
+
+module.exports = function check(str, bracketsConfig) {
+    let brackets = bracketsConfig.map(value => new BracketPair(value[0], value[1]));
+    let bracketQueue = [];
+    str.split("").forEach(value => {
+        let bracketPair = brackets.find(brackets => brackets.startBracket === value || brackets.endBracket === value);
+        if (!bracketPair) {
+            return false;
+        }
+        if (bracketPair.startBracket === bracketQueue[bracketQueue.length - 1] && bracketPair.endBracket === value && bracketQueue.length !== 0) {
+            bracketQueue.pop();
+        } else if (bracketPair.startBracket === value || bracketQueue.length === 0) {
+            bracketQueue.push(value);
+        }
+    });
+    return bracketQueue.length === 0;
+};
+
